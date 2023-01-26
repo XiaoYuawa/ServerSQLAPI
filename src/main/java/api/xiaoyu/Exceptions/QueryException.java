11f8ccef817918d1e55246d10026eb69f2ac7ef2
XiaoYuTwT <83535768+XiaoYuawa@users.noreply.github.com>
2023-01-26 17:07:48 +0800

    Add files via upload

---
+++ src/main/java/api/xiaoyu/Exceptions/QueryException.java
@@ -0,0 +1,10 @@
+package api.xiaoyu.Exceptions;
+
+public class QueryException extends RuntimeException{
+
+    public QueryException(){}
+    public QueryException(String message){
+        super(message);
+    }
+
+}
